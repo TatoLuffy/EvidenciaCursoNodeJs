b4a8e003fead8504e2426865ac4c4988832665f6
Daniel Bart <dbartolomester@gmail.com>
2019-12-27 18:04:06 +0100

    Module 5 - use of require in file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const os = require('os');
 const fs = require('fs');
 
+const mi = require('./mito.js');
+
 let cpu = os.cpus();
 let sistema = os.platform();
 let usuario = os.hostname();
@@ -9,10 +11,10 @@ let usuario = os.hostname();
 // console.log(sistema);
 // console.log(usuario);
 
-let cpu_string = JSON.stringify(cpu);
+/*let cpu_string = JSON.stringify(cpu);
 fs.appendFile('miarchivoprueba.txt',`Información del cpu: ${cpu_string} `, function(error){
     if (error){
         console.log('Error al crear archivo de prueba');
     }
-});
+});*/
 
