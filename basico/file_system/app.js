8227b79be67c066724b551d4bc66d9c126c8f24b
Daniel Bart <dbartolomester@gmail.com>
2019-12-29 11:21:02 +0100

    Module 7 File System - Prueba de fs rename asincrono en data_renombrado.txt

--- basico/file_system/app.js
+++ basico/file_system/app.js
@@ -1,9 +1,21 @@
 const fs = require("fs");
 
-fs.readFile('data.txt', "utf-8", (error,data)=> {
+console.log("Iniciado");
+
+/*fs.readFile('data.txt', "utf-8", (error,data)=> {
     if (error){
         console.log(`Error: ${console.error()}`);
     } else {
         console.log(data);
     }
 } );
+*/
+
+let data = fs.readFileSync('data.txt', 'utf-8');
+console.log(data);
+console.log("Finalizado");
+
+fs.rename('data.txt', 'data_renombrado.txt', (error) => {
+    if(error) throw error;
+    console.log('Se ha renombrado!!');
+});
