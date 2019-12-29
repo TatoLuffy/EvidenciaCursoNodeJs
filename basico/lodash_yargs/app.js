b647d60be97988ea26b93052378c1414c4d5d6d0
Daniel Bart <dbartolomester@gmail.com>
2019-12-29 13:31:49 +0100

    Parte 9 Lodash y Yargs. Enviar valores por terminal al programa app.js de lodash_yargs

--- basico/lodash_yargs/app.js
+++ basico/lodash_yargs/app.js
@@ -15,5 +15,26 @@ console.log(resultado);
 
 //_.times(3, ()=> console.log("Repetición for"));
 
+/*
 let resultado = _.find(z, {nombre:"Birto"});
 console.log(resultado);
+*/
+
+//Valor de entrada al usuario, input tradicional
+let comando = process.argv;
+console.log(comando);
+
+let comando2 = process.argv[2];
+
+if (comando2 === 'usuario'){
+    if (process.argv[3] == 'DaniCode') {
+        let x = { "nombre": "Dan"}
+        let y = { "apodo": "Birto"}
+        let z = [
+            { nombre: "Dan", apellido: "General Bonaparte"},
+            { nombre: "Birto", apellido: "De Los Angeles", edad: 22}
+        ]
+        let resultado = _.find(z, {nombre:"Birto"});
+        console.log(resultado);
+    }
+}
