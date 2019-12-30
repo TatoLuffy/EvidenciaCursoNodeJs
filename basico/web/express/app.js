8d77d6d045c115c4077b6645c291c61808cdc294
Daniel Bart <dbartolomester@gmail.com>
2019-12-30 03:01:13 +0100

    Petición 15 express

---
+++ basico/web/express/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+// Revisar si esta logado el usuario
+let isLogin = () => true;
+
+let logger = (req,res,next) => {
+    console.log('Petición de tipo: ', req.method);
+    next();
+}
+
+let showIP = (req,res,next) => {
+    console.log('IP: 127.0.0.1');
+    next();
+}
+
+// Interceptador de las peticiones
+app.use((req, res, next) => {
+    if(isLogin()){
+        next();
+    } else {
+        res.send('No estas logeado');
+    }
+}, logger, showIP);
+
+//app.use(logger);
+
+// Ejemplo 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.put('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.delete('/', function (req, res) {
+    res.send('Hello World!');
+});
+  
+app.listen(3000, () => {
+console.log('Example app listening on port 3000!');
+});
